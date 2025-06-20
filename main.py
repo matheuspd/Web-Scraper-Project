d517ce42b91bec4a1bc8e63beca123d2985daa80
Dauboau <86164187+Dauboau@users.noreply.github.com>
2025-06-20 16:53:03 -0300

    Classes In Separate File

--- main.py
+++ main.py
@@ -5,32 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
-class Disciplina:
-    def __init__(self, codigo, nome, cred_aula, cred_trab, ch, ce, cp, atpa):
-        self.codigo = codigo
-        self.nome = nome
-        self.creditos_aula = int(cred_aula)
-        self.creditos_trabalho = int(cred_trab)
-        self.carga_horaria = int(ch)
-        self.carga_estagio = int(ce) if ce else 0
-        self.carga_cpcc = int(cp) if cp else 0
-        self.carga_atpa = int(atpa) if atpa else 0
-
-class Curso:
-    def __init__(self, nome, unidade, duracao_ideal, duracao_min, duracao_max):
-        self.nome = nome
-        self.unidade = unidade
-        self.duracao_ideal = int(duracao_ideal)
-        self.duracao_minima = int(duracao_min)
-        self.duracao_maxima = int(duracao_max)
-        self.obrigatorias = []
-        self.optativas_livres = []
-        self.optativas_eletivas = []
-
-class Unidade:
-    def __init__(self, nome):
-        self.nome = nome
-        self.cursos = []
+from classes import Curso, Disciplina, Unidade
 
 limite_unidades = int(input("Digite o número máximo de unidades a processar: "))
 
